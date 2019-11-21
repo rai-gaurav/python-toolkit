581e5517f2ff47dfd4f2b85ab7c8493167a5f396
grai <gauravrai7860@gmail.com>
2019-11-21 20:00:59 +0530

    Update py_jira.py

--- py_jira.py
+++ py_jira.py
@@ -2,3 +2,8 @@ from jira import JIRA
 # There is a Project key and Project id for different project
 
 # Different Issue type have different id. It may differ from organization to organization
+
+class PyJira:
+    def __init__():
+        options = {"server": "https://jira.atlassian.com"}
+        jira = JIRA(options)
