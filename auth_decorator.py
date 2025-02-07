b206d5534ba2b526b9b3f01d1e11706a47380e7f
sudo-batman <gauravrai7860@gmail.com>
2025-02-07 20:39:57 +0530

    Create auth_decorator.py

---
+++ auth_decorator.py
@@ -0,0 +1,28 @@
+from functools import wraps
+from flask import Flask, request, jsonify
+import jwt
+
+app = Flask(__name__)
+
+def authorize(f):
+    @wraps(f)
+    def auth_required(*args, **kwargs):
+            if not 'Authorization' in request.headers:
+               return jsonify({"message": "Authorization token missing"}), 401
+
+            user = None
+            data = request.headers['Authorization'].encode('ascii','ignore')
+            token = str.replace(str(data), 'Bearer ','')
+            try:
+                user = jwt.decode(token, JWT_SECRET, algorithms=['HS256'])['sub']
+            except:
+                return jsonify({"message": "Token mismatch"}), 401
+
+            return f(user, *args, **kwargs)            
+    return auth_required
+
+@app.route('/api/create', methods=['POST'])
+@authorize
+def create(user):
+    data = json.loads(request.data)
+   
